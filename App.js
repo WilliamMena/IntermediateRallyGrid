0e0734f5b37a89dfbe6590e907eb97105352f962
WilliamMena <williammena1991@gmail.com>
2020-08-09 17:06:32 -0400

    Intermediate Grid not ready

--- App.js
+++ App.js
@@ -3,30 +3,58 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     launch: function() {
 
-        console.log("Our first App!");
-        this._loadData();
+        console.log("Our second App!");
+        // this._loadData();
+        this._loadIteractions();
     },
 
+    _loadIteractions: function() {
+        this.iterComboBox = Ext.create('Rally.ui.combobox.IterationComboBox', {
+            listeners: {
+               ready: function(combobox) {
+                   this._loadData();
+                },
+                select: function(combobox, records) {
+                    this._loadData();
+                },
+                scope: this
+            },
+        });
+        
+        this.add(this.iterComboBox);
+    },
+    
     _loadData: function() {
-        var myStore = Ext.create('Rally.data.wsapi.Store', {
-            model: 'User Story',
+        let selectedIterRef = this.iterComboBox.getRecord().get('_ref');
+
+        console.log('selected iter', selectedIterRef);
+
+        let myStore = Ext.create('Rally.data.wsapi.Store', {
+            model: 'Defect',
             autoLoad: true,
+            filters: [
+                {
+                    property: 'Iteration',
+                    opertation: '=',
+                    value: selectedIterRef
+                }
+            ],
             listeners: {
                 load: function(myStore, myData, success) {
                     this._loadGrid(myStore);
                 },
                 scope: this
             },
-            fetch: ['FormattedID', 'Name', 'ScheduleState']
+            fetch: ['FormattedID', 'Name', 'Severity', 'Iteration']
         });
     },
 
     _loadGrid: function(myStoryStore) {
 
-        var myGrid = Ext.create('Rally.ui.grid.Grid', {
+        let myGrid = Ext.create('Rally.ui.grid.Grid', {
             store: myStoryStore,
             columnCfgs: [
-                'FormattedID', 'Name', 'ScheduleState'
+                'FormattedID', 'Name', 'Severity', 'Iteration'
             ]
         });
 
